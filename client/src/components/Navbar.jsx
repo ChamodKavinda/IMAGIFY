3aacb34e7a8fb1a7a9dc331fcfecb2078b59e5e5
chamod kavinda <kavindachamod100@gmail.com>
2024-12-22 10:36:18 +0530

    set logo on navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+function Navbar() {
+  return (
+    <div>
+      <img src={assets.logo} alt="" className='w-28' />
+    </div>
+  )
+}
+
+export default Navbar
