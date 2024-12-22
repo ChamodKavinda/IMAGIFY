e9b5bc2d18bbb3a4d194c38a3e56ee337cce4788
chamod kavinda <kavindachamod100@gmail.com>
2024-12-22 11:02:02 +0530

    create pricing and login button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
+
+    const [user,setUser] = useState(null);
+
   return (
+    <div className='flex items-center justify-between py-4'>
+      <Link to='/'><img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40' />
+      </Link>
+
     <div>
-      <img src={assets.logo} alt="" className='w-28' />
+        {
+        user ?
+        <div></div>
+        :
+        <div className='flex items-center gap-2 sm:gap-5'>
+            <p className='cursor-pointer'>Pricing</p>
+            <button className='bg-zinc-800 text-white px-10 py-2 sm:px-2 text-sm rounded-full'>Login</button>
+        </div>
+        }
+        
+        
+    </div>
+
     </div>
   )
 }
