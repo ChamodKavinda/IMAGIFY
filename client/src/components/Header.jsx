50b33fcee3c7bfbb3c95ee341e6993d428764820
chamod kavinda <kavindachamod100@gmail.com>
2024-12-26 22:42:51 +0530

    implement onClickHandler

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { assets } from '../assets/assets';
 import {motion} from "framer-motion"
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+
+
+  const {user,setShowLogin} = useContext(AppContext);
+  const navigate = useNavigate()
+
+  const onClickHandler=()=>{
+
+    if(user){
+      navigate('/result')
+    }else{
+      setShowLogin(true)
+    }
+  }
+
   return (
     <motion.div className='flex flex-col justify-center items-center text-center my-20'
     initial={{opacity:0.2, y:100}}
@@ -35,7 +52,7 @@ const Header = () => {
       >
         Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.
       </motion.p>
-      <motion.button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
+      <motion.button onClick={onClickHandler} className='sm:text-lg text-white bg-black w-auto mt-20 px-12 py-2.5 flex items-center gap-2 rounded-full mb-20'
       whileHover={{scale: 1.05}}
       whileTap={{scale:0.95}}
       initial={{opacity:1}}
@@ -63,7 +80,7 @@ const Header = () => {
         initial={{opacity:0}}
         animate={{opacity:1}}
         transition={{delay:1.2 , duration:0.8}}
-        className='mt-2 text-neutral-600'>Generated images from imagify</motion.p>
+        className='mt-2 text-neutral-600 '>Generated images from imagify</motion.p>
 
     </motion.div>
   );
