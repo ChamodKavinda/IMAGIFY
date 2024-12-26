0bfb21b4ca9cdac7f61ba90cf5617ef4db3ceba8
chamod kavinda <kavindachamod100@gmail.com>
2024-12-26 22:55:37 +0530

    import useState

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,13 @@
-import { createContext } from "react";
+import { createContext,useState } from "react";
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props)=>{
     const [user,setUser] = useState(null);
+    const [showLogin,setShowLogin] = useState(false);
 
     const value = {
-        user,setUser
+        user,setUser,showLogin,setShowLogin
     }
 
     return(
