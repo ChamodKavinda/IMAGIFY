61c7af24b413eaec08bf6389635fc071bcb9be6b
chamod kavinda <kavindachamod100@gmail.com>
2025-01-11 22:17:09 +0530

    add generate image context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext,useEffect,useState } from "react";
 import { toast } from "react-toastify";
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
@@ -12,19 +13,39 @@ const AppContextProvider = (props)=>{
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
+    const navigate = useNavigate()
 
     const loadCreditsData = async ()=>{
         try {
             const {data} = await axios.get(backendUrl + '/api/user/credits',{headers:{token}})
 
             if(data.success){
-                setCredit(data.credit)
+                setCredit(data.credits)
                 setUser(data.user)
             }
         } catch (error) {
             console.log(error);
             toast.error(error.message);
             
+        }
+    }
+
+    const generateImage = async (prompt)=>{
+        try {
+            const {data} = await axios.post(backendUrl + '/api/image/generate-image', {prompt},{headers:{token}})
+
+            if(data.success){
+                loadCreditsData()
+                return data.resultImage
+            }else{
+                toast.error(data.message)
+                loadCreditsData()
+                if(data.creditBalance === 0){
+                    navigate('/buy')
+                }
+            }
+        } catch (error) {
+            toast.error(error.message)
         }
     }
 
@@ -41,7 +62,7 @@ const AppContextProvider = (props)=>{
     },[token])
 
     const value = {
-        user,setUser,showLogin,setShowLogin,backendUrl,token,setToken,credit,setCredit,loadCreditsData,logout
+        user,setUser,showLogin,setShowLogin,backendUrl,token,setToken,credit,setCredit,loadCreditsData,logout,generateImage
     }
 
 
